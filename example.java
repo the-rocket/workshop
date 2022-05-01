8e083dc4132d269b50d88a90b9b68d3ada9e2db9
EminAktash <eaktash2016@litsey2.ru>
2022-05-01 18:08:37 -0500

    I added new print line

--- example.java
+++ example.java
@@ -1,5 +1,6 @@
 class Example {
     public static void main(String[] args) {
-        System.out.println("Hello World!"); // Display the string.
+        System.out.println("Hello World!");
+        System.out.println("Hello Luqman!"); // Display the string.
     }
 }
